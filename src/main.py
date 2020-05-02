c90cd5a6e5db0ac2f99c54365dfbc33b938a9648
Yuchen Wang <wyc8094@gmail.com>
2020-05-02 17:46:17 +0800

    implement_color_hist

---
+++ src/main.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Yuchen Wang
+
+import cv2 as cv
+import numpy as np
+
+from features.color_histogram import ColorHistogram
+
+def main():
+    colorHist = ColorHistogram(1, 1, 256)
+
+    image = cv.imread('/Users/apple/Desktop/AIMango_sample/sample_image/D-Plant2_0610_3.jpg', 1)
+
+    features = colorHist.extract(image, normalize = True)
+
+    print(features.shape)
+
+main()
