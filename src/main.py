523cde95aa950398916e59decc088f783155edd9
Wong Yuchen <wyc8094@gmail.com>
2020-05-19 22:12:39 +0800

    AlexNet Experiment (#4)
    - Implement AlexNet with layer normalization
    - Some helper functions for keras layers
    - pretrained weights

--- src/main.py
+++ src/main.py
@@ -3,9 +3,12 @@
 import cv2 as cv
 import numpy as np
 import pandas as pd
+import random as rd
+import tensorflow as tf
 
 from features.color_histogram import ColorHistogram
 from features.gamma import Gamma
+from models.alexnet import AlexNet
 from models.svm import SVM
 
 def getLabelIndex(label):
@@ -28,7 +31,7 @@ def extractFeatures(labelFile, pathPrefix):
 
     X = np.empty([row, 2*4*4*16*3])
     Y = np.zeros([row], dtype=np.int16)
-    for i in range(row):
+    for i in range(10):
         imageId = df['image_id'][i]
         label = df['label'][i]
         Y[i] = getLabelIndex(label)
@@ -40,16 +43,65 @@ def extractFeatures(labelFile, pathPrefix):
         X[i][0:768] = colorHist.extract(image1, normalize=True)
         X[i][768:1536] = colorHist.extract(image2, normalize=True)
         X[i] /= np.linalg.norm(X[i])
+
+
         print(pathPrefix+imageId)
 
-    return X, Y
+    return np.array(X), Y
 
+def loadAllImages(labelFile, prefix, argument=False):
+    df = pd.read_csv(labelFile)
+    row, col = df.shape
+
+    resize = (227, 227)
+    X = []
+    Y = []
+    gamma1 = Gamma(1.5)
+    for i in range(row):
+        imageId = df['image_id'][i]
+        label = df['label'][i]
+        y = getLabelIndex(label)
+        image = cv.imread(prefix+imageId, 1)
+        image = gamma1.extract(image)
+        image = cv.resize(image, resize).astype(np.float64)
+        image /= 255.0
+        image[:,:,0] = image[:,:,0] - 0.485
+        image[:,:,1] = image[:,:,1] - 0.456
+        image[:,:,2] = image[:,:,2] - 0.406
+
+        sample = rd.uniform(0.0, 1.0)
+        if sample < 0.3 and argument == True:
+            flipedImage = cv.flip(image, -1)
+            X.append(flipedImage)
+            Y.append(y)
+
+        X.append(image)
+        Y.append(y)
+        print(prefix+imageId)
+
+    return np.array(X), tf.keras.utils.to_categorical(np.array(Y))
 
 def main():
-#    trainLabelFile = '/tmp2/yucwang/data/mongo/train.csv'
-#    trainPrefix = '/tmp2/yucwang/data/mongo/C1-P1_Train/'
-#    validLabelFile = '/tmp2/yucwang/data/mongo/dev.csv'
-#    validPrefix = '/tmp2/yucwang/data/mongo/C1-P1_Dev/'
+    trainLabelFile = '/tmp2/yucwang/data/mongo/train.csv'
+    trainPrefix = '/tmp2/yucwang/data/mongo/C1-P1_Train/'
+    validLabelFile = '/tmp2/yucwang/data/mongo/dev.csv'
+    validPrefix = '/tmp2/yucwang/data/mongo/C1-P1_Dev/'
+
+    trainX, trainY = loadAllImages(trainLabelFile, trainPrefix, argument=True)
+    testX, testY = loadAllImages(validLabelFile, validPrefix)
+
+    validIndicies = np.random.choice(testX.shape[0], 75)
+    validX = testX[validIndicies]
+    validY = testY[validIndicies]
+
+    print('Training with {} images.'.format(trainY.shape[0]))
+
+    model = AlexNet(inputShape=[227, 227, 3],
+            pretrainedWeights="./pretrained/alexnet_weights.h5")
+    model.train(weightsSavePath = './bin/exp9/', 
+            batches=6500, batchSize=128, learningRate=0.001, X=trainX, 
+            Y=trainY, validX=validX, validY=validY, decayStep=[2400, 5000])
+    model.evaluate(testX, testY)
 #
 #    trainX, trainY = extractFeatures(trainLabelFile, trainPrefix)
 #    validX, validY = extractFeatures(validLabelFile, validPrefix)
@@ -59,19 +111,19 @@ def main():
 #    np.save('./val_x.npy', validX)
 #    np.save('./val_y.npy', validY)
 
-    trainX = np.load('./bin/exp2/train_x.npz.npy')
-    trainY = np.load('./bin/exp2/train_y.npz.npy')
-    validX = np.load('./bin/exp2/val_x.npz.npy')
-    validY = np.load('./bin/exp2/val_y.npz.npy')
-
-    model = SVM(penalty='l2', loss='squared_hinge',
-            C=0.85, maxIter=2000)
-    print("SVM: Training get started.")
-    model.train(trainX, trainY)
-
-    print("SVM: Validation get started.")
-    acc, metrics = model.valid(validX, validY, classNum=3)
-    print(acc)
-    print(metrics)
+#    trainX = np.load('./bin/exp2/train_x.npz.npy')
+#    trainY = np.load('./bin/exp2/train_y.npz.npy')
+#    validX = np.load('./bin/exp2/val_x.npz.npy')
+#    validY = np.load('./bin/exp2/val_y.npz.npy')
+#
+#    model = SVM(penalty='l2', loss='squared_hinge',
+#            C=0.85, maxIter=2000)
+#    print("SVM: Training get started.")
+#    model.train(trainX, trainY)
+#
+#    print("SVM: Validation get started.")
+#    acc, metrics = model.valid(validX, validY, classNum=3)
+#    print(acc)
+#    print(metrics)
 
 main()
