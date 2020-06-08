4b3591f045540ead4d454980991de7b19b228b06
Yuchen Wong <yucwang@cml25.csie.ntu.edu.tw>
2020-06-08 08:51:13 +0800

    remove_unused_parts

--- src/models/inception_resnet.py
+++ src/models/inception_resnet.py
@@ -3,60 +3,6 @@
 import numpy as np
 import tensorflow as tf
 
-class ConvLN(tf.keras.layers.Layer):
-    def __init__(self, kernelY, kernelX, filters, stridesY, strideX, **kwrags):
-        super().__init__(**kwrags)
-
-        self._conv = conv(kernelY, kernelX, filters, stridesY, strideX, activation=None)
-        self._activation = tf.keras.layers.LeakyReLU(alpha=0.1)
-
-    def call(self, inputs):
-        res = self._conv(inputs)
-        res = self._activation(res)
-
-        return res
-
-
-class Inception(tf.keras.layers.Layer):
-    def __init__(self, params, concatAxis=-1, **kwrags):
-        super().__init__(**kwrags)
-
-        self._branch0 = params[0]
-        self._branch1 = params[1]
-        self._branch2 = params[2]
-        self._branch3 = params[3]
-
-        self._concatAxis = concatAxis
-
-        self._pathway0Conv = ConvLN(1, 1, self._branch0[0], 1, 1)
-
-        self._pathway1Conv1 = ConvLN(1, 1, self._branch1[0], 1, 1)
-        self._pathway1Conv2 = ConvLN(3, 3, self._branch1[1], 1, 1)
-
-        self._pathway2Conv1 = ConvLN(1, 1, self._branch2[0], 1, 1)
-        self._pathway2Conv2 = ConvLN(1, 5, self._branch2[1], 1, 1) 
-        self._pathway2Conv3 = ConvLN(5, 1, self._branch2[1], 1, 1)
-                
-
-        self._pathway3Conv1 = ConvLN(1, 3, self._branch3[0], 1, 1)
-        self._pathway3Conv2 = ConvLN(3, 1, self._branch3[0], 1, 1)
-
-    def call(self, inputs):
-        pathway0 = self._pathway0Conv(inputs)
-
-        pathway1 = self._pathway1Conv1(inputs)
-        pathway1 = self._pathway1Conv2(pathway1)
-
-        pathway2 = self._pathway2Conv1(inputs)
-        pathway2 = self._pathway2Conv2(pathway2)
-        pathway2 = self._pathway2Conv3(pathway2)
-
-        pathway3 = self._pathway3Conv1(inputs)
-        pathway3 = self._pathway3Conv2(pathway3)
-
-        return tf.keras.layers.concatenate([pathway0, pathway1, pathway2, pathway3],
-                axis=self._concatAxis)
-
 class InceptionResnet(object):
     def __init__(self, 
             inputShape=[299, 299, 3],
@@ -148,25 +94,3 @@ class InceptionResnet(object):
 
         print("accuracy: ", acc/row)
         print("metrics: ", metrics)
-            
-
-def conv(kernelHeight, kernelWidth, filters, strideY, strideX, activation='relu', padding='SAME', 
-        kernelRegularizer=None, name=None):
-    return tf.keras.layers.Conv2D(filters=filters, kernel_size=[kernelHeight, kernelWidth],
-            strides=[strideY, strideX], padding=padding, activation=activation, 
-            kernel_regularizer=kernelRegularizer, bias_initializer='glorot_uniform', name=name)
-
-def dropOut(dropOutProb):
-    return tf.keras.layers.Dropout(dropOutProb)
-
-def flatten():
-    return tf.keras.layers.Flatten()
-
-def fullConnect(numIn, numOut, name, activation='relu'):
-    return tf.keras.layers.Dense(numOut, input_shape=(numIn,),
-            activation=activation, bias_initializer='glorot_uniform', name=name)
-
-def maxPool(kernelHeight, kernelWidth,
-        strideY, strideX, padding='SAME', name=None):
-    return tf.keras.layers.MaxPool2D(pool_size=(kernelHeight, kernelWidth),
-            strides=(strideY, strideX), padding=padding, name=name)
