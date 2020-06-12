7a3532d85e73d5eb7dc658b7273adf579c5f9a92
r08922152 <r08922152@linux1.csie.ntu.edu.tw>
2020-06-12 23:47:34 +0800

    Data augmentation. Including gamma correction, flip, rotation and zoom in.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import random as rd
 import tensorflow as tf
+import imutils
 
 #from PIL import Image
 #from PIL import ImageFilter
@@ -54,7 +55,51 @@ def extractFeatures(labelFile, pathPrefix):
 
     return np.array(X), Y, filenameList
 
-def loadAllImages(labelFile, prefix, argument=False):
+def normalize(data):
+    data = np.array(data)
+    data /= 255.0
+    data[:,:,:,0] = data[:,:,:,0] - np.mean(data[:,:,:,0])
+    data[:,:,:,1] = data[:,:,:,1] - np.mean(data[:,:,:,1])
+    data[:,:,:,2] = data[:,:,:,2] - np.mean(data[:,:,:,2])
+
+    return data
+
+def dataAugment(image, y, resize):
+    aug_X = []
+    aug_Y = []
+    sample1 = rd.uniform(0.0, 1.0)
+    if sample1 < 0.2:
+        gamma2 = Gamma(1.2)
+        image = gamma2.extract(image)
+    if sample1 > 0.3 and sample1 < 0.5:
+        gamma2 = Gamma(1.4)
+        image = gamma2.extract(image)
+    if sample1 > 0.8:
+        gamma2 = Gamma(0.9)
+        image = gamma2.extract(image)
+
+    sample2 = rd.uniform(0.0, 1.0)
+    if sample2 < 0.4:
+        flipedImage = cv.flip(image, -1)
+        flipedImage = cv.resize(flipedImage, resize).astype(np.float64)
+        aug_X.append(flipedImage)
+        aug_Y.append(y)
+    if sample2 > 0.3 and sample2 < 0.7:
+        sample3 = rd.uniform(0.0, 1.0)
+        rotatedImage = imutils.rotate(image, angle=15*np.sign(sample3-0.5))
+        rotatedImage = cv.resize(rotatedImage, resize).astype(np.float64)
+        aug_X.append(rotatedImage)
+        aug_Y.append(y)
+    if sample2 > 0.6:
+        h, w, channel = image.shape
+        croppedImage = image[int(0.1*h):int(0.9*h), int(0.1*w):int(0.9*w), :]
+        croppedImage = cv.resize(croppedImage, resize).astype(np.float64)
+        aug_X.append(croppedImage)
+        aug_Y.append(y)
+
+    return aug_X, aug_Y
+
+def loadAllImages(labelFile, prefix, augment=False):
     df = pd.read_csv(labelFile)
     row, col = df.shape
 
@@ -62,7 +107,7 @@ def loadAllImages(labelFile, prefix, argument=False):
     X = []
     Y = []
     filenameList = []
-    gamma1 = Gamma(1.28)
+    gamma1 = Gamma(1)
     for i in range(row):
         imageId = df['image_id'][i]
         label = df['label'][i]
@@ -70,24 +115,21 @@ def loadAllImages(labelFile, prefix, argument=False):
         image = cv.imread(prefix+imageId, 1)
         image = gamma1.extract(image)
 
-        if row > col:
+        if image.shape[0] > image.shape[1]:
             image = cv.rotate(image, cv.ROTATE_90_CLOCKWISE)
+
+        if augment == True:
+            aug_X, aug_Y = dataAugment(image, y, resize)
+            X.extend(aug_X)
+            Y.extend(aug_Y)
+
         image = cv.resize(image, resize).astype(np.float64)
-        image /= 255.0
-        image[:,:,0] = image[:,:,0] - 0.264
-        image[:,:,1] = image[:,:,1] - 0.294
-        image[:,:,2] = image[:,:,2] - 0.506
-
-        sample = rd.uniform(0.0, 1.0)
-        if sample < 0.4 and argument == True:
-            flipedImage = cv.flip(image, -1)
-            X.append(flipedImage)
-            Y.append(y)
-
         X.append(image)
         Y.append(y)
         filenameList.append(prefix+imageId)
         print(prefix+imageId)
+
+    X = normalize(X)
 
     return np.array(X), tf.keras.utils.to_categorical(np.array(Y)), filenameList
 
@@ -97,7 +139,7 @@ def main():
     validLabelFile = '/tmp2/yucwang/data/mongo_data/dev.csv'
     validPrefix = '/tmp2/yucwang/data/mongo_data/C1-P1_Dev/'
 
-    trainX, trainY, trainFiles = loadAllImages(trainLabelFile, trainPrefix, argument=True)
+    trainX, trainY, trainFiles = loadAllImages(trainLabelFile, trainPrefix, augment=True)
     testX, testY, files = loadAllImages(validLabelFile, validPrefix)
 
     validIndicies = np.random.choice(testX.shape[0], 75)
